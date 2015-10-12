c9c36eef9b950309956f7915d015514a10639e0e
Bliss <blysxaye@icloud.com>
2015-10-12 17:34:27 +0700

    Initial Commit

---
+++ OpenHouseOneFive/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  OpenHouseOneFive
+//
+//  Created by Bliss Watchaye on 2015-10-12.
+//  Copyright © 2015 Thinc. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, QRCodeReaderViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Good practice: create the reader lazily to avoid cpu overload during the
+    // initialization and each time we need to scan a QRCode
+    lazy var reader = QRCodeReaderViewController(metadataObjectTypes: [AVMetadataObjectTypeQRCode])
+    
+    @IBAction func scanAction(sender: AnyObject) {
+        if QRCodeReader.supportsMetadataObjectTypes() {
+            reader.modalPresentationStyle = .FormSheet
+            reader.delegate               = self
+            
+            reader.completionBlock = { (result: String?) in
+                print("Completion with result: \(result)")
+            }
+            
+            presentViewController(reader, animated: true, completion: nil)
+        }
+        else {
+            let alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - QRCodeReader Delegate Methods
+    
+    func reader(reader: QRCodeReaderViewController, didScanResult result: String) {
+        self.dismissViewControllerAnimated(true, completion: { [unowned self] () -> Void in
+            let alert = UIAlertController(title: "QRCodeReader", message: result, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Cancel, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            })
+    }
+    
+    func readerDidCancel(reader: QRCodeReaderViewController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+
+}
+
