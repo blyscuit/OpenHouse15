bc989231b638c37443dc6b929ec9c302f60cb748
inactivelight <iww.shinigami@gmail.com>
2015-10-20 11:21:43 +0700

    Now with map

---
+++ OpenHouseOneFive/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  OpenHouseOneFive
+//
+//  Created by Bliss Wetchaye on 2015-10-20.
+//  Copyright © 2015 Thinc. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+            longitude: 151.20, zoom: 6)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
