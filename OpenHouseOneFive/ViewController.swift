e01bee5738800fb0e2909baa99c7ed52788cf4ba
Bliss <blysxaye@icloud.com>
2015-10-21 17:16:28 +0700

    New bingo

--- OpenHouseOneFive/ViewController.swift
+++ OpenHouseOneFive/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITableViewDataSource,UITableViewDelegate,UICollectionViewDelegate,UICollectionViewDataSource {
+class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITableViewDataSource,UITableViewDelegate {
     
     @IBOutlet weak var tableMain: UITableView!
-    @IBOutlet weak var bingoTable: UICollectionView!
     
     @IBOutlet weak var qrInstructionLabel: UILabel!
-    let titleText = ["NOW EVENT","CHECK IN"]
+    
+    var bingoCell:BingoViewController!
+    
+    let titleText = ["NOW EVENT","CHECK IN","Visited faculties","Locked faculties -- Please check in to unlock"]
     let titleImage = ["now-event-icon.png","check-in-icon.png"]
     
     var bingoBoard:Bingo!
@@ -26,32 +28,12 @@ class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITab
         tableMain.delegate = self
         tableMain.dataSource = self
         
-        bingoTable.delegate = self
-        bingoTable.dataSource = self
         bingoBoard = Bingo(filename: "Level_5")
 //        bingoTable.frame.size = CGSizeMake(bingoTable.frame.size.width, bingoTable.frame.size.width)
         
 //        bingoTable.reloadData()
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: (self.view.frame.size.width-60)/5, height:(self.view.frame.size.width-60)/5)
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
-        bingoTable.collectionViewLayout = layout
-        
-        setQRText()
     }
 
-    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: (self.view.frame.size.width-60)/5, height:(self.view.frame.size.width-60)/5)
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
-        bingoTable.collectionViewLayout = layout
-        
-        bingoTable.reloadData()
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -100,13 +82,13 @@ class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITab
     
     func numberOfSectionsInTableView(tableView: UITableView)
         -> Int {
-            return 2;
+            return 4;
     }
     
     func tableView(tableView: UITableView,
         numberOfRowsInSection section: Int)
         -> Int {
-            if(section == 0){
+            if(section == 0||section == 1){
                 return 1
             }
             return 0
@@ -116,13 +98,21 @@ class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITab
         cellForRowAtIndexPath indexPath: NSIndexPath)
         -> UITableViewCell {
             //            let user = self.sections[indexPath.section].users[indexPath.row]
-            
+            if(indexPath.section==0){
             let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
             
             cell.detailTextLabel?.text = "Tile"
             cell.textLabel?.text = "Time am"
             
             return cell
+            }else if(indexPath.section==1){
+                var cell = tableView.dequeueReusableCellWithIdentifier("bingoCell", forIndexPath: indexPath) as! BingoViewController
+                cell.start()
+                return cell
+            }else{
+                var cell = tableView.dequeueReusableCellWithIdentifier("facultyStatusCell", forIndexPath: indexPath) as UITableViewCell
+                return cell
+            }
     }
     
     /* section headers
@@ -142,68 +132,33 @@ class ViewController: UIViewController, QRCodeReaderViewControllerDelegate,UITab
     //            return name
     //    }
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if(section<=1){
         let  headerCell = tableView.dequeueReusableCellWithIdentifier("cellHeader") as? TitleTableViewCell
         headerCell?.titleLabel?.text = titleText[section]
-        headerCell?.titleImage.image = UIImage(named: titleImage[section])
-        return headerCell
+        
+            headerCell?.titleImage.image = UIImage(named: titleImage[section])
+            return headerCell
+        }else{
+            let  headerCell = tableView.dequeueReusableCellWithIdentifier("facultyCell") as? TitleTableViewCell
+            headerCell?.titleLabel?.text = titleText[section]
+            
+            return headerCell
+        }
     }
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 22;
+        if(indexPath.section==0){
+            return 22;
+        }else if(indexPath.section==1){
+            return 700;
+        }else{
+            return 58
+        }
     }
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 58
     }
     
-    // MARK: - collection view data source
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5*5
-    }
-    
-    func collectionView(collectionView: UICollectionView,
-        cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-            
-            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("stamp", forIndexPath: indexPath) as! BingoCollectionViewCell
-            cell.facultyImage.backgroundColor = UIColor.randomColor()
-            
-//             Customize cell height
-            cell.frame = CGRectMake(cell.frame.origin.x, cell.frame.origin.y,collectionView.frame.size.width/5, collectionView.frame.size.height/5)
-            return cell
-    }
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        return CGSize(width: (self.view.frame.size.width-60)/5, height:(self.view.frame.size.width-60)/5)
-    }
-    func collectionView(collectionView: UICollectionView,
-        shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-            return true
-    }
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        collectionView.deselectItemAtIndexPath(indexPath, animated: true)
-        print((collectionView.cellForItemAtIndexPath(indexPath) as! BingoCollectionViewCell).facultyImage.backgroundColor)
-    }
-    
-    func setQRText(){
-        var myMutableString = NSMutableAttributedString()
-        myMutableString = NSMutableAttributedString(string: "Explore more faculty and scan QR Code to unlock a prize.")
-        // NSMutableAttributedString(
-            //string: "Explore more faculty and scan QR Code to unlock a prize.",
-            //attributes: [NSFontAttributeName:UIFont(
-            //    name: "Georgia",
-            //    size: 18.0)!])
-        //Add more attributes here
-        
-            myMutableString.addAttribute(NSFontAttributeName,
-                value: UIFont.boldSystemFontOfSize(13),
-                range: NSRange(
-                    location: 25,
-                    length: 12))
-        
-        //Apply to the label
-        qrInstructionLabel.attributedText = myMutableString
-    }
 //    func collectionView(collectionView: UICollectionView,
 //        layout collectionViewLayout: UICollectionViewLayout,
 //        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
