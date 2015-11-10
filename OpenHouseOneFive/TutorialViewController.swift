ed62fcf654a90c51e48cb2f5d9d935dd1bf37758
Bliss <blysxaye@icloud.com>
2015-11-10 13:15:33 +0700

    tutorial

---
+++ OpenHouseOneFive/TutorialViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialViewController.swift
+//  OpenHouseOneFive
+//
+//  Created by Bliss Watchaye on 2015-11-10.
+//  Copyright © 2015 Thinc. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    @IBAction func startPress(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            
+        }
+    }
+}
